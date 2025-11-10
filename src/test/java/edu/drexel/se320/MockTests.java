859ec15bd14fb0b119415a0f933ac0648bbe3a24
Samii Shabuse <sshabuse@gmail.com>
2025-11-09 19:17:04 -0500

    test: add 2 more tests

--- src/test/java/edu/drexel/se320/MockTests.java
+++ src/test/java/edu/drexel/se320/MockTests.java
@@ -39,4 +39,63 @@ public class MockTests {
         verify(sc, never()).requestFileContents(anyString());
         assertNull(result);
     }
+
+    @Test
+    public void testSuccessfulDownload() throws IOException {
+        ServerConnection sc = mock(ServerConnection.class);
+        when(sc.connectTo("server")).thenReturn(true);
+        when(sc.requestFileContents("file.txt")).thenReturn(true);
+        when(sc.moreBytes()).thenReturn(true, false);
+        when(sc.read()).thenReturn("Hello");
+
+        Client c = new Client(sc);
+        String result = c.requestFile("server", "file.txt");
+
+        verify(sc).connectTo("server");
+        verify(sc).requestFileContents("file.txt");
+        verify(sc).read();
+        verify(sc).closeConnection();
+
+        assertEquals("Hello", result);
+    }
+
+    @Test
+    public void testInvalidFileReturnsNull() throws IOException {
+        ServerConnection sc = mock(ServerConnection.class);
+        when(sc.connectTo("server")).thenReturn(true);
+        when(sc.requestFileContents("nofile.txt")).thenReturn(false);
+
+        Client c = new Client(sc);
+        String result = c.requestFile("server", "nofile.txt");
+
+        verify(sc).closeConnection();
+        assertNull(result);
+    }
+
+    @Test
+    public void testMultipleReadsConcatenated() throws IOException {
+        ServerConnection sc = mock(ServerConnection.class);
+        when(sc.connectTo(anyString())).thenReturn(true);
+        when(sc.requestFileContents(anyString())).thenReturn(true);
+        when(sc.moreBytes()).thenReturn(true, true, false);
+        when(sc.read()).thenReturn("A", "B");
+
+        Client c = new Client(sc);
+        String result = c.requestFile("server", "file");
+
+        verify(sc, times(2)).read();
+        assertEquals("AB", result);
+    }
+
+    @Test
+    public void testIOExceptionDuringConnect() throws IOException {
+        ServerConnection sc = mock(ServerConnection.class);
+        when(sc.connectTo(anyString())).thenThrow(new IOException());
+
+        Client c = new Client(sc);
+        assertNull(c.requestFile("server", "file"));
+    }
+
+    @Test
+    
 }
