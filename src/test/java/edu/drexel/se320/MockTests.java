64708072081bb1892d49c1e63912fdae4a07ec66
Samii Shabuse <sshabuse@gmail.com>
2025-11-12 21:42:56 -0500

    test: fix comment back in top of test file and add back test code

--- src/test/java/edu/drexel/se320/MockTests.java
+++ src/test/java/edu/drexel/se320/MockTests.java
@@ -1,3 +1,38 @@
+/**
+ * SE320 Homework 3: Object Dependencies
+ * 
+ * Refactoring Explanation:
+ * 
+ * In the original version of this class, the Client directly instantiated an
+ * anonymous ServerConnection inside the requestFile() method. Which made the
+ * Client tightly coupled to the ServerConnection implementation and prevented it from
+ * being tested independently of an actual network connection, which caused problems for the client.
+ *
+ * To make the Client testable and remove this hard dependency, I refactored
+ * the class to use **constructor injection**, also known as the
+ * **Passing the Collaborator** pattern discussed in lecture slides. (also given by homework instructions)
+ *
+ * This approach exposes the dependency by requiring a ServerConnection
+ * instance to be passed into the Client constructor. Then the Client calls
+ * methods on ServerConnection: connectTo, requestFileContents, moreBytes,
+ * read, and closeConnection. And this is done without knowing or controlling its concrete type.
+ *
+ * During the mock testing process, a mock implementation of ServerConnection is injected using
+ * the Mockito framework (stated by the homework instructions). This allows all behaviors to be verified without 
+ * actually calling an actual server or network dependency.
+ *
+ * I chose this approach instead of alternatives like a collaborator factory or
+ * service locator because constructor injection keeps the design very simple,
+ * explicit, and fully compatible with unit testing frameworks if there were any. 
+ * It also makes the dependency requirements of Client clear and enforces proper separation
+ * between Client and ServerConnection.
+ *
+ * Homework Requirement: I left the original function behavior of not closing
+ * the connection when an invalid file is requested to ensure that my tests
+ * can detect the same bugs described in the homework assignment requirements.
+ */
+
+
 package edu.drexel.se320;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,6 +70,17 @@ public class MockTests {
 
          verify(mockedList).add("one");
          verify(mockedList).clear();
+    }
+
+    @Test
+    public void testServerConnectionFailureGivesNull() throws IOException {
+        ServerConnection sc = mock(ServerConnection.class);
+        Client c = new Client(sc);
+        when(sc.connectTo(anyString())).thenReturn(false);
+
+        // If you change the code to pass the mock above to the client (based on your choice of
+        // refactoring), this test should pass.  Until then, it will fail.
+        assertNull(c.requestFile("DUMMY", "DUMMY"));
     }
 
     /**
